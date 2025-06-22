fa5f046a7e7837b09ada30de18fdf131ae9ffb95
shaunak-g <shaunakgoswami44@gmail.com>
2025-06-22 21:18:12 +0530

    Update snakewg_.py

--- snakewg_.py
+++ snakewg_.py
@@ -1,1 +1,29 @@
+import random
+
+
+computer = random.choice([0,-1,1])
+youstr=input("Enter your choice :")
+youDict={"s":1,"w":-1,"g":0}
+reverseDict={1:"Snake",-1:"Water",0:"Gun"}
+you=youDict[youstr]
+
+print(f"You chose {reverseDict[you]} \n Computer chose {reverseDict[computer]}")
+
+if(computer==you):
+    print("It's a tie!")
+else:
+    if(computer == -1 and you == 1):
+        print("You Win !")
+    elif(computer== 1 and you ==0):
+        print("You Win !")
+    elif(computer== 0 and you ==-1):
+        print("You Win !") 
+    elif(computer== 1 and you ==-1):
+        print("You Lose !")
+    elif(computer== -1 and you ==0):
+        print("You Lose !")    
+    elif(computer== 0 and you ==1):
+        print("You Lose !")         
+    else:
+        print("Invalid Input")
  
